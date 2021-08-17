d7237faed33bef8b6378f1249c0219b422e9b14a
IT Common <it-21@Globegroup.com>
2021-08-17 14:51:33 +0600

    code reduce

--- js/banking.js
+++ js/banking.js
@@ -1,75 +1,70 @@
 // common function 
-function getInputValue() {
-    const depositInput = document.getElementById('deposit-input');
+function getInputValue(inputBal) {
+    const depositInput = document.getElementById(inputBal);
     const depositString = depositInput.value;
     const depositVal = parseFloat(depositString);
     depositInput.value = '';
     return depositVal;
 }
 
-function getInnerTextValue() {
-    const depositTotal = document.getElementById('deposit-total');
+function getInnerTextValue(innerTextBal) {
+    const depositTotal = document.getElementById(innerTextBal);
     const depositTotalText = depositTotal.innerText;
     const depositTotalBal = parseFloat(depositTotalText);
     return depositTotalBal;
 }
 
-function setNewTotalBal() {
-    const newBal = document.getElementById('deposit-total');
-    return newBal;
+function setNewTotalBal(newBal) {
+    const bal = document.getElementById(newBal);
+    return bal;
+}
+
+function getCurrentBal(currentBalNew) {
+    const current = document.getElementById(currentBalNew);
+    const currentText = current.innerText;
+    const currentBal = parseFloat(currentText);
+    return currentBal;
+
 }
 
 
 //deposit
 document.getElementById('deposit-btn').addEventListener('click', function () {
 
-    // const depositInput = document.getElementById('deposit-input');
-    // const depositString = depositInput.value;
-    // const depositValue = parseFloat(depositString);
-    const depositValue = getInputValue();
-    const depositTotalVal = getInnerTextValue();
-    // const depositTotal = document.getElementById('deposit-total');
-    // const depositTotalText = depositTotal.innerText;
-    // const depositTotalVal = parseFloat(depositTotalText);
+    const depositValue = getInputValue('deposit-input');
+    const depositTotalVal = getInnerTextValue('deposit-total');
 
     const newDepositBal = depositTotalVal + depositValue;
-    setNewTotalBal().innerText = newDepositBal;
-    //console.log(newDepositBal);
-    //depositTotal.innerText = newDepositBal;
-
+    setNewTotalBal('deposit-total').innerText = newDepositBal;
 
     // update balance
-    const current = document.getElementById('current-bal');
-    const currentBalText = current.innerText;
-    const newCurrent = parseFloat(currentBalText);
-    let currentNewBal = newCurrent + depositValue;
-    console.log(currentNewBal);
-    current.innerText = currentNewBal;
 
-    // depositInput.value = '';
+    const newCurrent = getCurrentBal('current-bal');
+    let currentNewBal = newCurrent + depositValue;
+    setNewTotalBal('current-bal').innerText = currentNewBal;
 
 })
 
 //withdraw 
 document.getElementById('withdraw-btn').addEventListener('click', function () {
 
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawText = withdrawInput.value;
-    const withDraw = parseFloat(withdrawText);
+    const witdrawValue = getInputValue('withdraw-input');
+    const withdrawVal = getInnerTextValue('withdraw-total');
 
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const withdrawTotalText = withdrawTotal.innerText;
-    const withdrawTotalVal = parseFloat(withdrawTotalText);
+
+    const withDraw = witdrawValue;
+    const withdrawTotalVal = withdrawVal;
 
     const newTotalWithDraw = withDraw + withdrawTotalVal;
-    withdrawTotal.innerText = newTotalWithDraw;
+    setNewTotalBal('withdraw-total').innerText = newTotalWithDraw;
 
     const current = document.getElementById('current-bal');
     const currentText = current.innerText;
     const currentBal = parseFloat(currentText);
+
     const newCurrentBal = currentBal - withDraw;
     current.innerText = newCurrentBal;
 
-    withdrawInput.value = '';
+
 
 })
